a80bc0b1c52b48032c98dc72ece25ed68a94b940
lyn1dev <chosentaco68@gmail.com>
2025-08-03 04:23:49 +0800

    Initial commit

---
+++ src/main/kotlin/me/lyn1/netherRaids/NetherRaids.kt
@@ -0,0 +1,14 @@
+package me.lyn1.netherRaids
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NetherRaids : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
